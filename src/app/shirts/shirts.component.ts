c97b7211cb9673a32a8f076cacfe87b288428ecb
jonasa64 <hvidovrefifa@gmail.com>
2020-04-17 20:23:11 +0200

    Created t-shrit component

---
+++ src/app/shirts/shirts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ShritService } from '../services/shirts.service'
+
+@Component({
+  selector: 'app-shirts',
+  templateUrl: './shirts.component.html',
+  styleUrls: ['./shirts.component.css']
+})
+export class ShirtsComponent implements OnInit {
+  shirts: any;
+  constructor(private shritServic: ShritService) { }
+
+  ngOnInit(): void {
+    this.retrieveShirts();
+  }
+
+  retrieveShirts() {
+    this.shritServic.getAll().subscribe(data => {
+      console.log(data);
+      this.shirts = data;
+    }, error => {
+      console.log(error);
+    }
+    );
+  }
+
+}
