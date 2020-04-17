464a372439213b7f053035e5ef142ca397adbf14
jonasa64 <hvidovrefifa@gmail.com>
2020-04-18 01:15:20 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShirtsComponent } from '../app/shirts/shirts.component';
+import { ShirtComponent } from '../app/shirts/shirt/shirt.component';
+import { ShirtFormComponent } from '../app/shirts/shirt-form/shirt-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bla', component: ShirtsComponent },
+  { path: 'shirts/:id', component: ShirtComponent },
+  { path: 'add', component: ShirtFormComponent },
+  { path: 'add/:id', component: ShirtFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
