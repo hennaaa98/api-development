78e1bde86c0f6af34e8340f0bd6c868c6cf5751e
jonasa64 <hvidovrefifa@gmail.com>
2020-04-17 22:06:44 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShirtsComponent } from './shirts/shirts.component';
 import { ShritService } from '../app/services/shirts.service';
+import { ShirtFormComponent } from './shirts/shirt-form/shirt-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShirtsComponent
+    ShirtsComponent,
+    ShirtFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [ShritService],
   bootstrap: [AppComponent]
