35b618c6a6bb5d15183c3b9c050beef9f17860f4
jonasa64 <hvidovrefifa@gmail.com>
2020-04-17 22:06:48 +0200

    Update shirts.service.ts

--- src/app/services/shirts.service.ts
+++ src/app/services/shirts.service.ts
@@ -1,12 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+}
+
 
 
 @Injectable()
 
 export class ShritService {
 
+
     baseUrl = 'http://localhost:3000/shirts';
+
 
     constructor(private http: HttpClient) { }
 
@@ -19,7 +28,7 @@ export class ShritService {
     }
 
     create(data) {
-        return this.http.post(this.baseUrl, data);
+        return this.http.post(this.baseUrl, data, httpOptions);
     }
 
     update(id, data) {
@@ -27,7 +36,7 @@ export class ShritService {
     }
 
     delete(id) {
-        return this.http.delete(`${this.baseUrl}/delete/${id}`);
+        return this.http.delete(`${this.baseUrl}/delete/${id}`, httpOptions);
     }
 
 }
