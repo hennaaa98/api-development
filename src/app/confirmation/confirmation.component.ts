f4b1a65ebcad6df577f2db1d9b979881706167bf
jonasa64 <hvidovrefifa@gmail.com>
2020-04-22 23:39:52 +0200

    redirect methode to go customer orders

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-confirmation',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+
+
+  }
+
+  redirect() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    this.router.navigate(['orders', user.id]);
   }
 
 }
