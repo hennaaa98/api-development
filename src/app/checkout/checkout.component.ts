c15965ce872064f2660172e469dffb2c4643fac1
jonasa64 <hvidovrefifa@gmail.com>
2020-04-22 23:44:54 +0200

    Update checkout.component.ts

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CardValidator } from './cardValidtor';
+import { OrderService } from '../services/order.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -11,7 +12,7 @@ export class CheckoutComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBulider: FormBuilder, private router: Router) {
+  constructor(private formBulider: FormBuilder, private router: Router, private orderService: OrderService) {
     this.form = this.formBulider.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -38,9 +39,15 @@ export class CheckoutComponent implements OnInit {
     const user = JSON.parse(localStorage.getItem('user'));
     if (this.form.valid) {
       console.log('valid');
+
       const fd = new FormData();
       fd.append('customerId', user.id)
 
+      this.orderService.create(JSON.stringify(fd)).subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+      })
       this.router.navigate(['confirmation']);
     } else {
       console.log("invalid", this.form.get("creditCardNumber").hasError('invalidCreditCard'));
