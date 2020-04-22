9d06c56062189a358f8eef279392aef67770c85d
jonasa64 <hvidovrefifa@gmail.com>
2020-04-22 12:52:11 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ShirtComponent } from '../app/shirts/shirt/shirt.component';
 import { ShirtFormComponent } from '../app/shirts/shirt-form/shirt-form.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
-
+import { UpdateComponent } from './orders/update/update.component';
 
 const routes: Routes = [
   { path: 'shirt', component: ShirtsComponent },
@@ -13,7 +13,8 @@ const routes: Routes = [
   { path: 'add', component: ShirtFormComponent },
   { path: 'add/:id', component: ShirtFormComponent },
   { path: '', component: CustomerComponent },
-  { path: 'orders', component: OrdersComponent }
+  { path: 'orders', component: OrdersComponent },
+  { path: 'edit/:id', component: UpdateComponent }
 ];
 
 @NgModule({
