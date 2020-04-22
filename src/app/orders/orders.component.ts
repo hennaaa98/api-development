d464dc99f6f95444dd7c6b5750cb6560e901a024
Henna <qureshi.henna@gmail.com>
2020-04-23 01:34:52 +0200

    Cart and order

---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services/order.service';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+  orders: any;
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.findAll().subscribe(orders => {
+      this.orders = orders;
+      console.log(orders);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  delete(id) {
+    if (confirm('Are you sure you want to delete the order with id ' + id + ' ?')) {
+      this.orderService.delete(id).subscribe(order => {
+        console.log(order);
+      }, error => {
+        console.log(error);
+      })
+    }
+  }
+
+}
